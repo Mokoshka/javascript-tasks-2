46e0bfe639557350d9aa98bfe7e64f23cd8c1ba5
Rybalka Ekaterina <lonelygopnik@gmail.com>
2015-10-17 14:01:51 +0500

    Update phoneBook.js

--- phoneBook.js
+++ phoneBook.js
@@ -2,26 +2,64 @@
 
 var phoneBook = []; // Здесь вы храните записи как хотите
 var note;
-var rePhone = /^((8|\+?\d{1,3})[\- ]?)?(\(\d{3}\)|\d{3}[\- ]?)[\d\- ]{7,10}$/;
+var rePhone = /^(\+?(\d{1,3})[\- ]?)?((\(\d{3}\))|(\d{3}))[\- ]?([\d\- ]{7,10})$/;
 var reEmail = /^[-\w.]+@([A-z0-9А-Я][-A-z0-9А-Я]+\.)+[A-zА-Я]{2,4}$/i;
+var reNumb = /\D/ig;
 /*
    Функция добавления записи в телефонную книгу.
    На вход может прийти что угодно, будьте осторожны.
 */
 
 function checkPhone(phone) {
-    return rePhone.test(phone);
+    var match = rePhone.exec(phone);
+    if (match == null){
+        return ""
+    }
+    var format = "";
+
+    //Форматирование кода страны
+    if (match[2] == "8" || match[1] == undefined){
+        format += "+7";
+    } else {
+        format += "+" + match[2];
+    }
+
+    //Форматирование кода города
+    if (match[4] != undefined){
+        format += " " + match[4] + " ";
+    } else {
+        format += " (" + match[5] + ") ";
+    }
+
+    //Форматирование телефона
+    var number = "";
+    var char;
+    for (var i=0; i<match[6].length; i++){
+        char = match[6][i];
+        if (char != "-" && char != " "){
+            number += char;
+            if (number.length == 3 || number.length == 5){
+                number += "-";
+            }
+        }
+    }
+
+    return format + number;
 }
 
 function checkEmail(email) {
     return reEmail.test(email);
 }
 
+function printNote(index){
+    console.log(phoneBook[index].name + ", " + phoneBook[index].phone + ", " + phoneBook[index].email);
+}
+
 module.exports.add = function add(name, phone, email) {
 
     // Ваша невероятная магия здесь
-    //console.log(checkPhone(phone), );
-    if (checkPhone(phone) && checkEmail(email)) {
+    var resultPhone = checkPhone(phone);
+    if (resultPhone != "" && checkEmail(email) && name != "" && name != undefined) {
         note = {
             name: name,
             phone: phone,
@@ -44,17 +82,16 @@ module.exports.find = function find(query) {
     var i;
     if (query === undefined) {
         for (i=0; i<len; i++){
-            console.log(phoneBook[i].name + ", " + phoneBook[i].phone + ", " + phoneBook[i].email);
+            printNote(i);
         }
     } else {
-        var q = new RegExp(query);
+        var q = new RegExp(query.replace(reNumb, ""));
         for (i=0; i<len; i++){
-            if (q.test(phoneBook[i].name) || q.test(phoneBook[i].email) || q.test(phoneBook[i].phone)){
-                console.log(phoneBook[i].name + ", " + phoneBook[i].phone + ", " + phoneBook[i].email);
+            if (q.test(phoneBook[i].name) || q.test(phoneBook[i].email) || q.test(phoneBook[i].phone.replace(reNumb, ""))){
+                printNote(i);
             }
         }
     }
-
 };
 
 /*
@@ -63,11 +100,10 @@ module.exports.find = function find(query) {
 module.exports.remove = function remove(query) {
 
     // Ваша необьяснимая магия здесь
-    var len = phoneBook.length;
-    var q = new RegExp(query);
     if (query != undefined) {
-        for (var i=0; i<len; i++){
-            if (q.test(phoneBook[i].name) || q.test(phoneBook[i].email) || q.test(phoneBook[i].phone)){
+        var q = new RegExp(query.replace(reNumb, ""));
+        for (var i=phoneBook.length-1; i>=0; i--){
+            if (q.test(phoneBook[i].name) || q.test(phoneBook[i].email) || q.test(phoneBook[i].phone.replace(reNumb, ""))){
                 phoneBook.splice(i, 1);
             }
         }
@@ -94,3 +130,4 @@ module.exports.showTable = function showTable() {
     // Ваша чёрная магия здесь
 
 };
+
